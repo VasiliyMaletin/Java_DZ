603d7d875aa17c2b9bc73438eed6d14ca370c7ed
Vasiliy Maletin <maletinvasilii@mail.ru>
2023-01-27 22:33:11 +0800

    completed HW_3

---
+++ Evgeniy_Finogenov/seminar_3/HW/HW_3.java
@@ -0,0 +1,46 @@
+package seminar_3.HW;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
+
+public class HW_3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        
+        random(list1);
+        random(list2);
+        System.out.println("----------Рандомные числа----------");
+        System.out.println(list1);
+        System.out.println(list2);
+        System.out.println();
+        
+        list1.retainAll(list2);
+        System.out.println("----------Удаление чисел----------");
+        System.out.println(list1);
+        System.out.println();
+
+        Collections.sort(list1);
+        System.out.println("----------Сортировка через Collections----------");
+        System.out.println(list1);
+        System.out.println();
+
+        list2.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer num1, Integer num2) {
+                return num2 - num1;
+            }
+        });
+        System.out.println("----------Сортировка через Comparator----------");
+        System.out.println(list2);
+    }
+    
+    public static void random(ArrayList<Integer> list) {
+        Random rd = new Random();
+        for (int i = 0; i < 20; i++) {
+            list.add(rd.nextInt(50));
+        }
+    }
+}
